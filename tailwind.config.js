c753500b6383f999e0d7621e90c70632f56ba8b8
Ngqiqo <mphumzimathanjana@gmail.com>
2024-11-15 14:17:56 +0200

    fix tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
-import type { Config } from "tailwindcss";
-
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -17,21 +16,21 @@ export default {
       },
       keyframes: {
         bounceBlink: {
-          '0%, 100%': { transform: 'translateY(0)', opacity: '1' },
-          '50%': { transform: 'translateY(-10px)', opacity: '0.5' },
+          "0%, 100%": { transform: "translateY(0)", opacity: "1" },
+          "50%": { transform: "translateY(-10px)", opacity: "0.5" },
         },
         glitch: {
-          '0%': { textShadow: '0px 2px #FF3D00, 0px -2px #00DFFC' },
-          '50%': { textShadow: '02px -2px #FF3D00, 0px 2px #00DFFC' },
-          '100%': { textShadow: '0px 2px #FF3D00, 0px -2px #00DFFC' },
+          "0%": { textShadow: "0px 2px #FF3D00, 0px -2px #00DFFC" },
+          "50%": { textShadow: "02px -2px #FF3D00, 0px 2px #00DFFC" },
+          "100%": { textShadow: "0px 2px #FF3D00, 0px -2px #00DFFC" },
         },
       },
       animation: {
-        bounceBlink: 'bounceBlink 1s infinite',
-        glitch: 'glitch .5s ease-in-out infinite',
+        bounceBlink: "bounceBlink 1s infinite",
+        glitch: "glitch .5s ease-in-out infinite",
       },
     },
   },
   plugins: [],
   darkMode: "class",
-} satisfies Config;
+};
