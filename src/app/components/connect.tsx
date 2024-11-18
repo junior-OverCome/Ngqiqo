8d57f6a82a2b93c3d7b420be51c6d6e78e6ba4ca
Ngqiqo <mphumzimathanjana@gmail.com>
2024-11-18 08:45:57 +0200

    EmailJS install

--- src/app/components/connect.tsx
+++ src/app/components/connect.tsx
@@ -1,9 +1,118 @@
-import React from 'react'
+'use client';
+import React, { useState } from "react";
+import EmailJS from "@emailjs/browser";
 
-const connect = () => {
+
+const Connect = () =>{
+  const [formData, setFormData] = useState({
+    user_name: "",
+    user_email: "",
+    message: "",
+  });
+
+  const [status, setStatus] = useState<string | null>(null);
+  const [showForm, setShowForm] = useState(false);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) =>{
+    const {name, value} =e.target;
+    setFormData({ ...formData, [name]: value});
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setStatus(null);
+
+      try{
+        const res = await EmailJS.sendForm(
+          'service_dh4tkai',
+          'template_ec4ft7r',
+          e.target as HTMLFormElement,
+          'k_kO26QQCd1vAZFof'
+        );
+        if (res.status === 200)
+        setStatus("message sent successfully");
+        else 
+          setStatus ('Message not sent, please try again');
+        }catch (error) {
+          setStatus("An error occured. Please try again.");
+          console.error("EmailJS Error:", error);
+      }      
+  };
+
   return (
-    <div>connect</div>
+    <section className="flex flex-col items-center">
+      <div className='flex flex-col items-center justify-center mt-16 mb-8'>
+        <p className='text-xs font-semibold'>CONNECT</p>
+        <h1 className='font-bold'>LET'S CHAT</h1>
+        <p className='text-gray-500 text-sm sm:text-base mt-2'>Impressed yet? If yes, then let's connect!</p>
+      </div>
+    <div className="rounded-lg p-4 border bg-white max-w-full mx-auto">
+    {/* Button to toggle form visibility */}
+    <button onClick={()=> setShowForm(!showForm)} className="w-full py-2 px-4 bg-indigo-600 text-white hover:scale-105 transition-transform rounded-md">
+    {showForm ? "Close" : "Connect"}
+    </button>
+    {showForm &&(
+      <div>
+        <h2 className="text-2xl font-bold text-center mb-4">Connect</h2>
+        {status && <p className="text-center mb-4 text-green-500">{status}</p>}
+        <form id="contact-form" onSubmit={handleSubmit} className="space-y-4">
+          <div>
+            <label htmlFor="user_name" className="block text-sm font-medium text-gray-700">Name</label>
+            <input 
+            type="text"
+            id="user_name"
+            name="user_name"
+            value={formData.user_name}
+            onChange={handleChange}
+            required
+            placeholder="Enter your full-name"
+            className="w-full p-2 mt-1 rounded-md focus:outline-none focus:ring-2"
+            />
+          </div>
+          <div>
+            <label htmlFor="user_email" className="block text-sm font-medium text-gray-700">Email</label>
+            <input 
+            type="text"
+            id="user_email"
+            name="user_email"
+            value={formData.user_email}
+            onChange={handleChange}
+            required
+            placeholder="Enter your email"
+            className="w-full p-2 mt-1 rounded-md focus:outline-none focus:ring-2"
+            />
+          </div>
+          <div>
+            <label htmlFor="message" className="block text-sm font-medium text-gray-700">Message</label>
+            <textarea 
+            id="message"
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+            required
+            placeholder="What do yo wanna talk about?"
+            className="w-full p-2 mt-1 rounded-md focus:outline-none focus:ring-2"
+            />
+          </div>
+          <div className="justify-center flex">
+            <button type="submit" className="w-full py-2 px-4 text-white bg-indigo-600 focus:outline-none focus:ring-2">
+              Send
+            </button>
+          </div>
+        </form>
+      </div>
+    )}
+    </div>
+    <div className="flex flex-col items-center">
+      
+      <div className="flex flex-row gap-2 mt-4">
+        <a href="">LinkedIn</a>
+        <a href="">Github</a>
+      </div>
+      
+    </div>
+    </section>
   )
 }
 
-export default connect
+export default Connect;
--- src/app/components/projects.tsx
+++ src/app/components/projects.tsx
@@ -1,28 +1,133 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react';
 
-const projects = () => {
+function GoogleCard() {
   return (
-    <section className='flex flex-col h-full w-full justify-center items-center pt-16 my-16'>
-      <div className='flex flex-col items-center'>
-      <p className='text-xs font-semibold'>PROJECTS</p>
-        <h1 className='font-bold'>CRAFTING WITH CODE</h1>
-        <p className='text-gray-500 text-sm sm:text-base mt-2'>Projects that push my boundaries</p>
-      </div>
-      <div className='grid grid-cols-2 rounded-md my-12 px-8 grap-6 items-center justify-center w-full h-40 shadow-[0_35px_60px_-15px_rgba(0,6,5,0.3)]'>
-        <div className='justify-center items-center'>description</div>
-        <div className='flex flex-col items-center justify-center'>
-          <p>Project 1</p>
-          <p>Project 2</p>
-          <p>Project 3</p>
-          <p>Project 4</p>
-          <p>Project 5</p>
-          <button className='cursor-pointer bg-blue-300 w-full rounded-md px-2 '>project 6</button>
-          
-        </div>
-      </div>
+    <div className='p-4 bg-white flex flex-col gap-5'>
+      <p className='text-gray-500 font-Sora text-2xl'>Creating responsive and efficient applications with:</p>
+      <div className='grid grid-cols-4 gap-1'>
+        <div className='bg-black text-white items-center justify-center flex border-r-2 rounded-bl-md transition transform hover:scale-125 hover:font-semibold hover:mr-3'> React</div>
+        <div className='bg-black text-white items-center justify-center flex border-r-2 transition transform hover:scale-125 hover:font-semibold hover:mx-3'> Node.js</div>
+        <div className='bg-black text-white items-center justify-center flex border-r-2 transition transform hover:scale-125 hover:font-semibold hover:mx-3'> HTML</div>
+        <div className='bg-black text-white items-center justify-center flex border-r-2 rounded-tr-md transition transform hover:scale-125 hover:font-semibold hover:ml-3 h-10'> CSS</div>
 
-    </section>
-  )
+      </div>
+    </div>
+  );
 }
 
-export default projects
+function FacebookCard() {
+  return (
+    <div className='p-4 bg-white flex flex-col gap-5'>
+      <p className='text-gray-500 font-Sora text-2xl'>Ensuring seamless connectivity and secure network solutions to keep everything running smoothly.</p>
+      <div className='grid grid-cols-4 gap-1'>
+        <div className='bg-black text-white text-sm items-center justify-center flex border-r-2 rounded-bl-md transition px-4 transform hover:scale-125 hover:font-semibold hover:mr-3'>Configuration</div>
+        <div className='bg-black text-white text-sm items-center justify-center flex border-r-2 transition transform px-4 hover:scale-125 hover:font-semibold hover:mx-3'>Diagnosis</div>
+        <div className='bg-black text-white text-sm items-center justify-center flex border-r-2 transition transform px-4 hover:scale-125 hover:font-semibold hover:mx-3'> Security Protocols</div>
+        <div className='bg-black text-white text-sm items-center justify-center flex border-r-2 rounded-tr-md transition px-4 transform hover:scale-125 hover:font-semibold hover:ml-3 '> TCP/IP Routing</div>
+
+      </div>
+    </div>
+  );
+}
+
+function CabGoCard() {
+  return (
+    <div className='p-4 bg-white flex flex-col gap-5'>
+      <p className='text-gray-500 font-Sora text-2xl'>Ensuring seamless connectivity and secure network solutions to keep everything running smoothly.</p>
+      <div className='grid grid-cols-4 gap-1'>
+        <div className='bg-black text-white text-sm items-center justify-center flex border-r-2 rounded-bl-md transition px-4 transform hover:scale-125 hover:font-semibold hover:mr-3'>Configuration</div>
+        <div className='bg-black text-white text-sm items-center justify-center flex border-r-2 transition transform px-4 hover:scale-125 hover:font-semibold hover:mx-3'>Diagnosis</div>
+        <div className='bg-black text-white text-sm items-center justify-center flex border-r-2 transition transform px-4 hover:scale-125 hover:font-semibold hover:mx-3'> Security Protocols</div>
+        <div className='bg-black text-white text-sm items-center justify-center flex border-r-2 rounded-tr-md transition px-4 transform hover:scale-125 hover:font-semibold hover:ml-3 '> TCP/IP Routing</div>
+
+      </div>
+    </div>
+  );
+}
+
+function SwssrCard() {
+  return (
+    <div className='p-4 bg-white flex flex-col gap-5'>
+      <p className='text-gray-500 font-Sora text-2xl'>Ensuring seamless connectivity and secure network solutions to keep everything running smoothly.</p>
+      <div className='grid grid-cols-4 gap-1'>
+        <div className='bg-black text-white text-sm items-center justify-center flex border-r-2 rounded-bl-md transition px-4 transform hover:scale-125 hover:font-semibold hover:mr-3'>Configuration</div>
+        <div className='bg-black text-white text-sm items-center justify-center flex border-r-2 transition transform px-4 hover:scale-125 hover:font-semibold hover:mx-3'>Diagnosis</div>
+        <div className='bg-black text-white text-sm items-center justify-center flex border-r-2 transition transform px-4 hover:scale-125 hover:font-semibold hover:mx-3'> Security Protocols</div>
+        <div className='bg-black text-white text-sm items-center justify-center flex border-r-2 rounded-tr-md transition px-4 transform hover:scale-125 hover:font-semibold hover:ml-3 '> TCP/IP Routing</div>
+
+      </div>
+    </div>
+  );
+}
+
+export default function Projects() {
+  const [activeCard, setActiveCard] = useState<'google' | 'facebook' | 'cabgo' | 'swssr' | null>('google');
+
+  return (
+    <section className='flex flex-col items-center w-full h-full'>
+      <div className='flex flex-col items-center justify-center mt-16'>
+        <p className='text-xs font-semibold'>Projects</p>
+        <h1 className='font-bold'>EXPANDING TECH</h1>
+        <p className='text-gray-500 text-sm sm:text-base mt-2'>Projects that push my boundaries</p>
+      </div>
+      
+      <main className='mt-8 mb-16 pt-8 px-8 pb-6 flex flex-col md:flex-row items-center gap-4 h-full w-full rounded-md shadow-[0_35px_60px_-15px_rgba(0,6,5,0.3)]'>
+        {/* Left Side - Display Card */}
+        <div className={`w-full md:w-1/2 flex items-center ${!activeCard ? 'hidden' : ''} md:flex`}>
+          {activeCard === 'google' && <GoogleCard />}
+          {activeCard === 'facebook' && <FacebookCard />}
+          {activeCard === 'cabgo' && <CabGoCard />}
+          {activeCard === 'swssr' && <SwssrCard />}
+
+        </div>
+
+        {/* Right Side - Buttons */}
+        <div className='w-full md:w-1/2 flex flex-col gap-0'>
+          <button
+            onClick={() => setActiveCard('google')}
+            className='flex-1 text-black transition rounded-t-md bg-[#8e7f9b] hover:bg-[#b4a0da] font-semibold py-4'
+          >
+            Google
+          </button>
+
+          <button
+            onClick={() => setActiveCard('facebook')}
+            className='flex-1 bg-[#8e7f9b] hover:bg-[#b4a0da] text-black font-semibold py-4'
+          >
+            Facebook
+          </button>
+
+          <button
+            onClick={() => setActiveCard('cabgo')}
+            className='flex-1 bg-[#8e7f9b] hover:bg-[#b4a0da] text-black font-semibold py-4'
+          >
+            CabGo
+          </button>
+
+          <button
+            onClick={() => setActiveCard('swssr')}
+            className='flex-1 bg-[#8e7f9b] rounded-b-md hover:bg-[#b4a0da] text-black font-semibold py-4'
+          >
+            <div className='flex gap-1 items-center justify-center'>
+            <div className='animate-spin h-8 w-8  border-4 border-t-transparent border-blue-400 items-center justify-center rounded-full'></div>
+            <span className=' mt-4 font-semibold text-lg'>
+                Loading...
+                </span>
+            </div>
+          </button>
+
+          {/* Close Card Button */}
+          {activeCard && (
+            <button
+              onClick={() => setActiveCard('google')}
+              className='mt-4 bg-gray-500 uppercase text-white font-bold py-2 '
+            >
+              Close
+            </button>
+          )}
+        </div>
+      </main>
+    </section>
+  );
+}
