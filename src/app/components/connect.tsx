3c58eedd585b01692a2e5eeaf0f578c22852be7f
Ngqiqo <mphumzimathanjana@gmail.com>
2024-11-18 09:00:43 +0200

    user emailjs

--- src/app/components/connect.tsx
+++ src/app/components/connect.tsx
@@ -5,7 +5,7 @@ import EmailJS from "@emailjs/browser";
 
 const Connect = () =>{
   const [formData, setFormData] = useState({
-    from_name: "",
+    user_name: "",
     user_email: "",
     message: "",
   });
@@ -60,9 +60,9 @@ const Connect = () =>{
             <label htmlFor="user_name" className="block text-sm font-medium text-gray-700">Name</label>
             <input 
             type="text"
-            id="from_name"
+            id="user_name"
             name="user_name"
-            value={formData.from_name}
+            value={formData.user_name}
             onChange={handleChange}
             required
             placeholder="Enter your full-name"
