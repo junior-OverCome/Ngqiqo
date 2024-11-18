0cb39217f45e9fe912d6f7c669fe23137c7a7866
Ngqiqo <mphumzimathanjana@gmail.com>
2024-11-18 14:12:34 +0200

    projects styling

--- src/app/components/projects.tsx
+++ src/app/components/projects.tsx
@@ -83,39 +83,22 @@ export default function Projects() {
         </div>
 
         {/* Right Side - Buttons */}
-        <div className='w-full md:w-1/2 flex flex-col gap-0'>
-          <button
+        <ul className='w-full md:w-1/2 flex flex-col gap-0'>
+          <li className='flex flex-col items-center '>
+            <h2 className='uppercase font-bold text-3xl font-Sora'>ngqiqo portfolio</h2>
+            <div className='text-gray-400'>Personal portfolio that showcases skills and commitment to learning </div>
+            <a href="https://ngqiqo.vercel.app" 
+              className=' uppercase'
+            >View</a>
+          </li>
+          
+          {/* <button
             onClick={() => setActiveCard('google')}
             className='flex-1 text-black transition rounded-t-md bg-[#8e7f9b] hover:bg-[#b4a0da] font-semibold py-4'
           >
             Google
-          </button>
+          </button> */}
 
-          <button
-            onClick={() => setActiveCard('facebook')}
-            className='flex-1 bg-[#8e7f9b] hover:bg-[#b4a0da] text-black font-semibold py-4'
-          >
-            Facebook
-          </button>
-
-          <button
-            onClick={() => setActiveCard('cabgo')}
-            className='flex-1 bg-[#8e7f9b] hover:bg-[#b4a0da] text-black font-semibold py-4'
-          >
-            CabGo
-          </button>
-
-          <button
-            onClick={() => setActiveCard('swssr')}
-            className='flex-1 bg-[#8e7f9b] rounded-b-md hover:bg-[#b4a0da] text-black font-semibold py-4'
-          >
-            <div className='flex gap-1 items-center justify-center'>
-            <div className='animate-spin h-8 w-8  border-4 border-t-transparent border-blue-400 items-center justify-center rounded-full'></div>
-            <span className=' mt-4 font-semibold text-lg'>
-                Loading...
-                </span>
-            </div>
-          </button>
 
           {/* Close Card Button */}
           {activeCard && (
@@ -126,7 +109,7 @@ export default function Projects() {
               Close
             </button>
           )}
-        </div>
+        </ul>
       </main>
     </section>
   );
