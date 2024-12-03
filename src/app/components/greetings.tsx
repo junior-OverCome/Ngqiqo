c62b0b0ff9a8de2760771f67bc782be4709972a8
Ngqiqo <mphumzimathanjana@gmail.com>
2024-12-03 13:47:20 +0200

    greeting padding fix

--- src/app/components/greetings.tsx
+++ src/app/components/greetings.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const greetings = () => {
   return (
     <section className='flex flex-col items-center justify-center h-full pb-20 pt-20'>
-        <div  className='flex flex-col pt-32 items-center h-full'>
+        <div  className='flex flex-col pt-44 items-center h-full'>
             <h1 className='text-4xl font-bold pt-10'>Hi, I'm Ngqiqo.</h1>
             <p className='text-md pt-2 text-gray-500 '>Exploring Tech, One Project at a Time</p>
         </div>
